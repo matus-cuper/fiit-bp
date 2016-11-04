ec36d570f1da3c65805ed14baa814eb70bbbd631
matus-cuper <matus.cuper@gmail.com>
2016-11-04 21:40:05 +0100

    Added some R time series plots

--- scripts/r_notes.R
+++ scripts/r_notes.R
@@ -13,8 +13,24 @@ weekdays(tmp$DATUM)
 
 # names(tmp) return header
 
-# create plot from first 96 values in tmp variable
+# create plot from first 96 values in tmp variable - one day plot
 plot(head(tmp$CAS, 96), head(tmp$Suma_odbery, 96))
 
 # create time series from data
 plot(ts(head(tmp$Suma_odbery, 96)))
+
+# create one graph with several times series
+ts.plot(ts(head(tmp$Suma_odbery, 960), ts(head(tmp3$Suma_odbery, 960))))
+
+# the same thins as above
+x <- 0:96
+ts.plot(ts(tmp$Suma_odbery[x]), ts(tmp3$Suma_odbery[x]))
+
+# create 20 graphs of 20 weeks, after every week wait for input
+for (w in c(0:19)) {
+  a <- w*672 + 1
+  b <- (w+1) * 672
+  ts.plot(ts(tmp$Suma_odbery[a:b]), ts(tmp3$Suma_odbery[a:b]))
+  print(tmp$DATUM[a])
+  line <- readline()
+}

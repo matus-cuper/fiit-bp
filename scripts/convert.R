384c05328c0727e0906e028ed3b476f9700c48c9
matus-cuper <matus.cuper@gmail.com>
2017-02-20 10:14:43 +0100

    Added SVR

---
+++ scripts/convert.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+# CSV transformation
+
+args <- commandArgs(TRUE)
+
+if (length(args)!=2) {
+  args <- c("--help")
+}
+
+if("--help" %in% args) {
+  cat("Usage: convert.R SOURCE DEST
+ 
+      Arguments:
+      SOURCE    - source file to convert
+      DEST      - output file 
+      --help    - print this text
+ 
+      Example:
+      Rscript --vanilla convert.R old.csv new.csv\n")
+  
+  q(save="no")
+}
+
+# read data from csv
+fileContent <- read.csv(file = args[1], sep = ",", dec = ".", header = TRUE)
+
+# set date to usable normal format
+fileContent$DATUM <- as.Date(fileContent$DATUM, format = "%d/%m/%Y")
+
+# set time to float hours
+fileContent$CAS <- (fileContent$CAS - fileContent$CAS[1])/ 60 
+
+# set hours to normal time format 
+fileContent$TIME <- paste(floor(fileContent$CAS), ":", fileContent$CAS %% 1 * 60, ":00", sep = "")
+
+# create timestamp from DATUM and TIME
+fileContent$timtestamp <- strptime(paste(fileContent$DATUM, fileContent$TIME), "%Y-%m-%d %H : %M :%S")
+
+# remove fields
+fileContent$value <- fileContent$Suma_odbery
+fileContent$DATUM <- NULL
+fileContent$CAS <- NULL
+fileContent$Suma_odbery <- NULL
+fileContent$TIME <- NULL
+
+write.csv(file = args[2], fileContent, row.names = FALSE)

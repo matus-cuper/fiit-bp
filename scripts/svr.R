7711bb62e59167d90754a8ef50313ce6053292d0
matus-cuper <matus.cuper@gmail.com>
2017-02-20 10:14:43 +0100

    Added SVR

---
+++ scripts/svr.R
@@ -0,0 +1,60 @@
+## SVR with PSO
+
+# When measurement was performed, set matrix record to 1
+setHoursOnes <- function(m, iMax, jMax) {
+  for (i in 1:iMax) {
+    for (j in 1:jMax) {
+      m[(i - 1) * jMax + j, j] <- 1
+    }
+  }
+  return(m)
+}
+
+# When measurement was performed, set matrix day column to 1
+setDaysOnes <- function(d, m, iMax, jMax) {
+  for (i in 1:iMax) {
+    for (j in 1:jMax) {
+      m[(i - 1) * jMax + j, jMax + as.POSIXlt(d[(i - 1) * jMax+ j])$wday] <- 1
+    }
+  }
+  return(m)
+}
+
+
+
+dataRaw <- read.csv("~/r/fiit-bp/data/suma_do_maj2015/99_UPLNE_CONVERTED.csv", header = TRUE, sep = ",")
+
+measurementsPerDay <- 96
+daysPerWeek <- 7
+trainingSetSize <- 10
+testingSetSize <- 1
+trainingSetRecords <- measurementsPerDay * trainingSetSize
+testingSetRecords <- measurementsPerDay * testingSetSize
+
+trainingMatrix <- matrix(0, nrow = trainingSetRecords, ncol = measurementsPerDay + daysPerWeek)
+rownames(trainingMatrix) <- dataRaw$value[1:trainingSetRecords]
+colnames(trainingMatrix) <- c(seq(1, measurementsPerDay), seq(1, daysPerWeek))
+trainingMatrix <- setHoursOnes(trainingMatrix, trainingSetSize, measurementsPerDay)
+trainingMatrix <- setDaysOnes(dataRaw$timtestamp[1:trainingSetRecords], trainingMatrix, trainingSetSize, measurementsPerDay)
+
+stopifnot(trainingMatrix[1,1] == 1)
+stopifnot(trainingMatrix[96,96] == 1)
+stopifnot(trainingMatrix[1,97] == 1)
+stopifnot(trainingMatrix[96,97] == 1)
+stopifnot(trainingMatrix[97,98] == 1)
+
+testingMatrix <- matrix(0, nrow = testingSetRecords, ncol = measurementsPerDay + daysPerWeek)
+colnames(testingMatrix) <- c(seq(1, measurementsPerDay), seq(1, daysPerWeek))
+testingMatrix <- setHoursOnes(testingMatrix, testingSetSize, measurementsPerDay)
+testingMatrix <- setDaysOnes(dataRaw$timtestamp[(trainingSetRecords+1):(trainingSetRecords+1+testingSetRecords)], testingMatrix, testingSetSize, measurementsPerDay)
+
+stopifnot(testingMatrix[1,1] == 1)
+stopifnot(testingMatrix[96,96] == 1)
+stopifnot(testingMatrix[1,100] == 1)
+stopifnot(testingMatrix[96,100] == 1)
+
+#install.packages("kernlab")
+#library(kernlab)
+require(kernlab)
+
+predict(ksvm(trainingMatrix), testingMatrix)

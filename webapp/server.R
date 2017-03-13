d495a7383fca8b5ddeed44a35ad9cc6149dee269
matus-cuper <matus.cuper@gmail.com>
2017-03-13 23:08:42 +0100

    Nearly solved UI rendering

--- webapp/server.R
+++ webapp/server.R
@@ -3,11 +3,28 @@ source("plot.R")
 
 shinyServer(function(input, output) {
   
-  output$selectUI <- renderUI({ 
-    selectInput("partnerName", "Select your choice", list("raz", "dva", "tri"))
+  output$numberOfParameters <- renderUI({
+    parameters <- algorithmsList[[1]][2:3]
+    # textInput(paste("param", parameters[1], sep = ""), label = paste ("Parameter", parameters[1]), value = "ahoj")
+
+    lapply(1:2, function(i) {
+      textInput(
+        paste("param", parameters[i], sep = ""), label = paste ("Parameter", parameters[i]), value = "ahoj"
+        )
+    })
+    
+    
+    # for (p in parameters) {
+    #   print(p)
+    #   textInput(paste("param", p, sep = ""), label = paste ("Parameter", p), value = "ahoj")
+    # }
   })
   
-  output$random <- renderPlot({
-    test(input$parameter2)
-  })
+  # output$selectUI <- renderUI({ 
+  #   selectInput("partnerName", "Select your choice", list("raz", "dva", "tri"))
+  # })
+  
+  # output$random <- renderPlot({
+  #   test(10)
+  # })
 })

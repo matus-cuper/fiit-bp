ebdc49cebe0970be1a01d97fc207c1610f47877d
matus-cuper <matus.cuper@gmail.com>
2017-03-13 15:45:28 +0100

    Improved converter script

--- scripts/convert.R
+++ scripts/convert.R
@@ -21,6 +21,12 @@ if("--help" %in% args) {
   q(save="no")
 }
 
+setRightHour <- function(x) {
+  if (x < 0)
+    return(x + 24)
+  return(x)
+}
+
 # read data from csv
 fileContent <- read.csv(file = args[1], sep = ",", dec = ".", header = TRUE)
 
@@ -28,10 +34,20 @@ fileContent <- read.csv(file = args[1], sep = ",", dec = ".", header = TRUE)
 fileContent$DATUM <- as.Date(fileContent$DATUM, format = "%d/%m/%Y")
 
 # set time to float hours
-fileContent$CAS <- (fileContent$CAS - fileContent$CAS[1])/ 60 
+fileContent$CAS <- fileContent$CAS / 60
+
+# move to previous day if hours are negative
+for (i in 1:nrow(fileContent)) {
+  if (fileContent$CAS[i] < 0) {
+    fileContent$DATUM[i] <- fileContent$DATUM[i] - 1
+  }
+}
+
+# set hours like -1.0 to 23.0
+fileContent$CAS <- lapply(fileContent$CAS, setRightHour)
 
 # set hours to normal time format 
-fileContent$TIME <- paste(floor(fileContent$CAS), ":", fileContent$CAS %% 1 * 60, ":00", sep = "")
+fileContent$TIME <- paste(floor(as.numeric(fileContent$CAS)), ":", as.numeric(fileContent$CAS) %% 1 * 60, ":00", sep = "")
 
 # create timestamp from DATUM and TIME
 fileContent$timtestamp <- strptime(paste(fileContent$DATUM, fileContent$TIME), "%Y-%m-%d %H : %M :%S")

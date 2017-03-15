c8e59bcb8b8a46f4db06b4ed0b563ff87b341a47
matus-cuper <matus.cuper@gmail.com>
2017-03-15 21:26:21 +0100

    Cleaned shiny

--- scripts/03-svr-psoptim.R
+++ scripts/03-svr-psoptim.R
@@ -5,13 +5,13 @@ library(config)
 pathToConfig <- "~/r/fiit-bp/scripts/config.yml"
 conf03 <- config::get("03-svr-psoptim", file = pathToConfig)
 
-startTime <- Sys.time()
+# startTime <- Sys.time()
 result <- psoptim(FUN = svrOptimize, 
                   n = conf03$numberOfParticles, 
                   max.loop = conf03$maxIterations, 
                   xmin = c(conf03$minValues$C, conf03$minValues$epsilon), 
                   xmax = c(conf03$maxValues$C, conf03$maxValues$epsilon), 
                   anim = conf03$animation)
-Sys.time() - startTime
+# Sys.time() - startTime
 
 predictedValues <- svrCompute(trainingMatrix, testingMatrix, verificationData, accuracyFunction, result$sol[1], result$sol[2])

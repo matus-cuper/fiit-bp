8002d6614979d22e605b874b925ba5cc51e73550
matus-cuper <matus.cuper@gmail.com>
2017-03-27 19:27:57 +0200

    Added named parameters in do.call

--- scripts/03-svr-psoptim.R
+++ scripts/03-svr-psoptim.R
@@ -3,22 +3,30 @@
 library(psoptim)
 library(config)
 pathToConfig <- "~/r/fiit-bp/scripts/config.yml"
-config.svr.psoptim <- config::get("03-svr-psoptim", file = pathToConfig)
+conf03 <- config::get("03-svr-psoptim", file = pathToConfig)
 
 psoptimWraper <- function(fn, params) {
   return(psoptim::psoptim(FUN = fn,
-                 xmin = c(as.numeric(params[1]), as.numeric(params[2])),
-                 xmax = c(as.numeric(params[3]), as.numeric(params[4])),
-                 anim = config.svr.psoptim$animation))
+                          n = params$numberOfParticles,
+                          max.loop = params$maxIterations,
+                          xmin = params$xmin,
+                          xmax = params$xmax,
+                          anim = conf03$animation))
 }
+
+# Test func call
+# result <- psoptimWraper(svrErrorWrapper, c(list(numberOfParticles = 10,
+#                                                   maxIterations = 10,
+#                                                   xmin = c(0, 0),
+#                                                   xmax = c(2, 2))))
 
 # startTime <- Sys.time()
 # result <- psoptim(FUN = "svrErrorWrapper",
-#                   n = config.svr.psoptim$numberOfParticles,
-#                   max.loop = config.svr.psoptim$maxIterations,
-#                   xmin = c(config.svr.psoptim$minValues$C, config.svr.psoptim$minValues$epsilon),
-#                   xmax = c(config.svr.psoptim$maxValues$C, config.svr.psoptim$maxValues$epsilon),
-#                   anim = config.svr.psoptim$animation)
+#                   n = conf03$numberOfParticles,
+#                   max.loop = conf03$maxIterations,
+#                   xmin = c(conf03$minValues$C, conf03$minValues$epsilon),
+#                   xmax = c(conf03$maxValues$C, conf03$maxValues$epsilon),
+#                   anim = conf03$animation)
 # Sys.time() - startTime
 
 # predictedValues <- svrCompute(trainingMatrix, testingMatrix, verificationData, accuracyFunction, result$sol[1], result$sol[2])

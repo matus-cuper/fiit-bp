e367289ed675f473e4425346218d8beb0016d5e1
matus-cuper <matus.cuper@gmail.com>
2017-04-02 00:53:05 +0200

    Added ARIMa data reading

--- scripts/02-pso-optimize.R
+++ scripts/02-pso-optimize.R
@@ -1,14 +1,15 @@
-## optimalize PSO
+## optimalize PSO and return solution and result
+
 # example function call
 # result <- psoptimWraper(svrErrorWrapper, c(list(numberOfParticles = 10, maxIterations = 10, xmin = c(0.01, 0), xmax = c(2, 2))))
 # result <- do.call("psoptimWrapper", list(svrErrorWrapper, c(list(numberOfParticles = 10, maxIterations = 10, xmin = c(0.01, 0), xmax = c(2, 2)))))
 
 library(psoptim)
 library(config)
-pathToConfig <- "~/r/fiit-bp/scripts/config.yml"
-conf03 <- config::get("03-svr-psoptim", file = pathToConfig)
+pso.properties <- config::get("02-pso-optimize", file = pathToConfig)
 
-psoptimWrapper <- function(fn, params) {
+pso.optimizeFn <- function(params) {
+  
   return(psoptim::psoptim(FUN = fn,
                           n = params$numberOfParticles,
                           max.loop = params$maxIterations,
@@ -16,3 +17,8 @@ psoptimWrapper <- function(fn, params) {
                           xmax = params$xmax,
                           anim = conf03$animation))
 }
+
+
+# tmp <- svr.readDataFn("~/r/fiit-bp/data/cleaned/99_UPLNE_CONVERTED_11D.csv", 96, 0.9)
+# svr.predictFn(c(tmp, CToOptimize = 1, epsilonToOptimize = 2, errorFn = "mape"))
+# do.call("svr.predictFn", list( c(tmp, CToOptimize = 1, epsilonToOptimize = 2, errorFn = "mape") ))

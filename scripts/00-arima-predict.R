941e151b598247a29cc888cd95287409e30acfd1
matus-cuper <matus.cuper@gmail.com>
2017-04-02 18:36:11 +0200

    Added arima usage

--- scripts/00-arima-predict.R
+++ scripts/00-arima-predict.R
@@ -1,8 +1,8 @@
 ## compute ARIMA and return error
 
-# tmp <- svr.readDataFn("~/r/fiit-bp/data/cleaned/99_UPLNE_CONVERTED_11D.csv", 96, 0.9)
-# svr.predictFn(c(tmp, CToOptimize = 1, epsilonToOptimize = 2, errorFn = "mape"))
-# do.call("svr.predictFn", list( c(tmp, CToOptimize = 1, epsilonToOptimize = 2, errorFn = "mape") ))
+# tmp <- arima.readDataFn("~/r/fiit-bp/data/cleaned/99_UPLNE_CONVERTED_11D.csv", 96, 0.9)
+# arima.predictFn(c(tmp, pToOptimize = 1, dToOptimize = 1, qToOptimize = 1, errorFn = "mape"))
+# tmd <- do.call("arima.predictFn", list( c(tma, pToOptimize = 1, dToOptimize = 1, qToOptimize = 1, errorFn = "mape") ))
 
 library(forecast)
 

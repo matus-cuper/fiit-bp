6da5ed56ef367f6ea2e42dc2f10e7decc4f28906
matus-cuper <matus.cuper@gmail.com>
2017-04-02 18:19:07 +0200

    Added ARIMA prediciton

---
+++ scripts/00-arima-predict.R
@@ -0,0 +1,30 @@
+## compute ARIMA and return error
+
+# tmp <- svr.readDataFn("~/r/fiit-bp/data/cleaned/99_UPLNE_CONVERTED_11D.csv", 96, 0.9)
+# svr.predictFn(c(tmp, CToOptimize = 1, epsilonToOptimize = 2, errorFn = "mape"))
+# do.call("svr.predictFn", list( c(tmp, CToOptimize = 1, epsilonToOptimize = 2, errorFn = "mape") ))
+
+library(forecast)
+library(config)
+arima.properties <- config::get("00-arima-predict", file = pathToConfig)
+
+# Set ARIMA parameters, compute and return predicted values
+arima.predict <- function(params) {
+  arimaModel <- arima(x = params$trainingTimeSeries,
+                      order = c(p = params$pToOptimize,
+                                d = params$dToOptimize,
+                                q = params$qToOptimize))
+  return(forecast.Arima(arimaModel, h = 192))
+}
+
+# Used by optimization function
+arima.predictFn <- function(params) {
+  arimaModel <- arima(x = params$trainingTimeSeries,
+                      order = c(p = params$pToOptimize,
+                                d = params$dToOptimize,
+                                q = params$qToOptimize))
+  arimaForecast <- forecast.Arima(arimaModel, h = 192) 
+  # add testing TS ?
+  # add parameter h as testingTS size
+  return(do.call(params$errorFn, list(c(arimaForecast$mean), params$verificationData)))  
+}

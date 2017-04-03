ca70283a8fb8df724bd8073fbc09a15adc864491
matus-cuper <matus.cuper@gmail.com>
2017-04-03 09:43:36 +0200

    Added list creation based on optimization algorithm

--- webapp/00-read-parameters.R
+++ webapp/00-read-parameters.R
@@ -9,7 +9,24 @@ readPredictionParameters <- function(selectedAlgorithm) {
   
   names <- list()
   values <- list()
-  for (i in numberOfParameters) {
+  for (i in 1:numberOfParameters) {
+    names <- c(names, parameters[[i]]$id)
+    
+    value <- eval(parse(text = paste("input", "$", parameters[[i]]$id, sep = "")))
+    values <- c(values, value)
+  }
+  
+  result <- as.list(setNames(values, names))
+  return(result)
+}
+
+readOptimizationParameters <- function(selectedAlgorithm) {
+  numberOfParameters <- server.properties$optimizationAlgorithms[[selectedAlgorithm]]$numberOfOptimizationParameters
+  parameters <- server.properties$optimizationAlgorithms[[selectedAlgorithm]]$optimizationParameters
+  
+  names <- list()
+  values <- list()
+  for (i in 1:numberOfParameters) {
     names <- c(names, parameters[[i]]$id)
     
     value <- eval(parse(text = paste("input", "$", parameters[[i]]$id, sep = "")))
--- webapp/server.R
+++ webapp/server.R
@@ -40,34 +40,74 @@ function(input, output) {
       })
     )
   })
+  
+  
+  observeEvent({
+    input$submitComputation
+  }, {
+    output$resultValues <- renderText({
+      
+      selectedAlgorithm <- as.numeric(input$predictionAlgorithms)
+      numberOfParameters <- server.properties$predictionAlgorithms[[selectedAlgorithm]]$numberOfPredictionParameters
+      parameters <- server.properties$predictionAlgorithms[[selectedAlgorithm]]$predictionParameters
+        
+      names <- list()
+      values <- list()
+      for (i in 1:numberOfParameters) {
+        names <- c(names, parameters[[i]]$id)
+          
+        value <- eval(parse(text = paste("input", "$", parameters[[i]]$id, sep = "")))
+        values <- c(values, value)
+      }
+      params.prediction <- as.list(setNames(values, names))
+      
+      
+      selectedAlgorithm <- as.numeric(input$optimizationAlgorithms)
+      numberOfParameters <- server.properties$optimizationAlgorithms[[selectedAlgorithm]]$numberOfOptimizationParameters
+      parameters <- server.properties$optimizationAlgorithms[[selectedAlgorithm]]$optimizationParameters
+      
+      names <- list()
+      values <- list()
+      for (i in 1:numberOfParameters) {
+        names <- c(names, parameters[[i]]$id)
+        
+        value <- eval(parse(text = paste("input", "$", parameters[[i]]$id, sep = "")))
+        values <- c(values, value)
+      }
+      params.optimization <- as.list(setNames(values, names))
 
-
-  output$resultValues <- renderText({
-    inputFile <- input$inputFile
-    if (is.null(inputFile))
-      return(NULL)
-
-    readFunction <<- server.properties$predictionAlgorithms[[as.numeric(input$predictionAlgorithms)]]$readDataFunction
-    predictFunction <<- server.properties$predictionAlgorithms[[as.numeric(input$predictionAlgorithms)]]$predictFunction
-    optimizeFunction <<- server.properties$optimizationAlgorithms[[as.numeric(input$optimizationAlgorithms)]]$optimizeFunction
-
-    # Global assignment
-    preparedData <- do.call(readFunction, list(inputFile$datapath, input$measurementsPerDay, input$testDatasetProportion))
-
-    trainingMatrix <<- preparedData$trainingMatrix
-    testingMatrix <<- preparedData$testingMatrix
-    verificationData <<- preparedData$verificationData
-    accuracyFunction <<- server.properties$fitnessFunctions[[as.numeric(input$fitnessFunction)]]$accuracyFunction
-
-    reactiveComputation <<- reactive({ do.call(optimizeFunction, list(svrErrorWrapper, c(list(numberOfParticles = input$numberOfParticles,
-                                                                     maxIterations = input$maxIterations,
-                                                                     xmin = c(input$minC, input$minEpsilon),
-                                                                     xmax = c(input$maxC, input$maxEpsilon)))))
+      params.optimization$numberOfParticles
+      
     })
-
-    result <- reactiveComputation()
-    result$val
   })
+
+
+  # output$resultValues <- renderText({
+  #   inputFile <- input$inputFile
+  #   if (is.null(inputFile))
+  #     return(NULL)
+  # 
+  #   readFunction <<- server.properties$predictionAlgorithms[[as.numeric(input$predictionAlgorithms)]]$readDataFunction
+  #   predictFunction <<- server.properties$predictionAlgorithms[[as.numeric(input$predictionAlgorithms)]]$predictFunction
+  #   optimizeFunction <<- server.properties$optimizationAlgorithms[[as.numeric(input$optimizationAlgorithms)]]$optimizeFunction
+  # 
+  #   # Global assignment
+  #   preparedData <- do.call(readFunction, list(inputFile$datapath, input$measurementsPerDay, input$testDatasetProportion))
+  # 
+  #   trainingMatrix <<- preparedData$trainingMatrix
+  #   testingMatrix <<- preparedData$testingMatrix
+  #   verificationData <<- preparedData$verificationData
+  #   accuracyFunction <<- server.properties$fitnessFunctions[[as.numeric(input$fitnessFunction)]]$accuracyFunction
+  # 
+  #   reactiveComputation <<- reactive({ do.call(optimizeFunction, list(svrErrorWrapper, c(list(numberOfParticles = input$numberOfParticles,
+  #                                                                    maxIterations = input$maxIterations,
+  #                                                                    xmin = c(input$minC, input$minEpsilon),
+  #                                                                    xmax = c(input$maxC, input$maxEpsilon)))))
+  #   })
+  # 
+  #   result <- reactiveComputation()
+  #   result$val
+  # })
 
 
   output$resultPlot <- renderPlot({

0eb3e733f928ba2ec0e7599da20b0b51d0eac3af
matus-cuper <matus.cuper@gmail.com>
2017-04-03 17:25:54 +0200

    Test new function calling from view

--- scripts/02-pso-optimize.R
+++ scripts/02-pso-optimize.R
@@ -7,13 +7,39 @@
 library(psoptim)
 library(config)
 pso.properties <- config::get("02-pso-optimize", file = pathToConfig)
+server.properties <- config::get("server", file = pathToShinyConfig)
 
 pso.optimizeFn <- function(params) {
   
-  return(psoptim::psoptim(FUN = params$fn,
-                          n = params$numberOfParticles,
-                          max.loop = params$maxIterations,
-                          xmin = params$xmin,
-                          xmax = params$xmax,
-                          anim = conf03$animation))
+  selectedPredictionFn <- 1
+  selectedFitnessFn <- 1
+
+  params <- list(pathToFile = "~/r/fiit-bp/data/cleaned/99_UPLNE_CONVERTED_11D.csv",
+                 measurementsPerDay = 96,
+                 trainingSetProportion = 0.9,
+                 readDataFn = server.properties$predictionAlgorithms[[as.numeric(selectedPredictionFn)]]$readDataFn,
+                 predictFn = server.properties$predictionAlgorithms[[as.numeric(selectedPredictionFn)]]$predictFn,
+                 errorFn = server.properties$fitnessFunctions[[as.numeric(selectedFitnessFn)]]$errorFn)
+
+  params.optimization <- list(numberOfParticles = 10,
+                              maxIterations = 20,
+                              minC = 0.5,
+                              maxC = 1.5,
+                              minEpsilon = 0.05,
+                              maxEpsilon = 0.2)
+
+  preparedData <- do.call(readDataFn, list(pathToFile = params$pathToFile,
+                                           measurementsPerDay = params$measurementsPerDay,
+                                           trainingSetProportion = params$trainingSetProportion))
+
+  params.prediction <<- c(preparedData, "errorFn" = params$errorFn)
+
+  result <- psoptim::psoptim(FUN = eval(parse(text = predictFn)),
+                             n = params.optimization$numberOfParticles,
+                             max.loop = params.optimization$maxIterations,
+                             xmin = c(params.optimization$minC, params.optimization$minEpsilon),
+                             xmax = c(params.optimization$maxC, params.optimization$maxEpsilon),
+                             anim = pso.properties$animation)
+
+  return(list(minError = result$val, bestSolution = result$sol))
 }

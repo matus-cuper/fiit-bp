a70361865b377099ce888b2070ece756f37ac418
matus-cuper <matus.cuper@gmail.com>
2017-04-22 15:03:15 +0200

    Create converter for ARIMA

--- scripts/convert-dummy.R
+++ scripts/convert-dummy.R
@@ -9,14 +9,14 @@ if (length(args)!=2) {
 
 if("--help" %in% args) {
   cat("Usage: convert.R SOURCE DEST
- 
+      
       Arguments:
       SOURCE    - source file to convert
       DEST      - output file 
       --help    - print this text
- 
+      
       Example:
-      Rscript --vanilla convert.R old.csv new.csv\n")
+      Rscript --vanilla convert-dummy.R old.csv new.csv\n")
   
   q(save="no")
 }
@@ -36,15 +36,8 @@ fileContent$DATUM <- as.Date(fileContent$DATUM, format = "%d/%m/%Y")
 # set time to float hours
 fileContent$CAS <- fileContent$CAS / 60
 
-# move to previous day if hours are negative
-for (i in 1:nrow(fileContent)) {
-  if (fileContent$CAS[i] < 0) {
-    fileContent$DATUM[i] <- fileContent$DATUM[i] - 1
-  }
-}
-
-# set hours like -1.0 to 23.0
-fileContent$CAS <- lapply(fileContent$CAS, setRightHour)
+# set time to right position
+fileContent$CAS <- fileContent$CAS + abs(min(fileContent$CAS))
 
 # set hours to normal time format 
 fileContent$TIME <- paste(floor(as.numeric(fileContent$CAS)), ":", as.numeric(fileContent$CAS) %% 1 * 60, ":00", sep = "")

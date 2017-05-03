5a37c40b71508642baf38ec27735d69f70e9797d
matus-cuper <matus.cuper@gmail.com>
2017-05-03 20:01:48 +0200

    Update plots

--- scripts/rf-sample-preparation.R
+++ scripts/rf-sample-preparation.R
@@ -1,9 +1,9 @@
 # Draw plot with legend of sample data preparation for randomForest
 
-source("~/r/fiit-bp/cuper-bp/src/00-read-data.R")
-source("~/r/fiit-bp/cuper-bp/src/01-rf-prepare.R")
-path.app.conf <- "~/r/fiit-bp/cuper-bp/conf/app-config.yml"
-preparedData <- data.prepare(pathToFile = "~/r/fiit-bp/data/cleaned/99_UPLNE_CONVERTED_11D.csv", measurementsPerDay = 96,
+source("src/00-read-data.R")
+source("src/01-rf-prepare.R")
+path.app.conf <- "conf/app-config.yml"
+preparedData <- data.prepare(pathToFile = "data/99_UPLNE_CONVERTED_11D.csv", measurementsPerDay = 96,
                              trainingSetRange = c("2013-07-01", "2013-07-10"), testingSetRange = c("2013-07-09", "2013-07-11"))
 
 matplot(data.frame(rf.setSinForDay(preparedData$testingData$timestamp, preparedData$measurementsPerDay),
@@ -14,3 +14,9 @@ matplot(data.frame(rf.setSinForDay(preparedData$testingData$timestamp, preparedD
 
 legend("bottomleft", legend=c("sin(d)", "cos(d)", "sin(w)", "cos(w)"), pch=20, col=c(1, 2, 3, 4), horiz=TRUE)
 
+
+matplot(data.frame(log(rf.nthInDay(preparedData$testingData$timestamp, preparedData$measurementsPerDay), base = preparedData$measurementsPerDay),
+                   log(rf.nthInWeek(preparedData$testingData$timestamp, 7), base = 7)),
+        type = c("l"), col = 1:length(preparedData$testingData$value), ylab = "Hodnota", xlab = "Pocet meraní")
+
+legend("bottomright", legend=c("d - meranie v dni", "w - den  v tyzdni"), pch=20, col=c(1, 2, 3, 4), horiz=TRUE)

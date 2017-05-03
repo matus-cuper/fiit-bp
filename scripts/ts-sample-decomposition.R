04b55aee1a9df23a58a0faf2069443506b07fde4
matus-cuper <matus.cuper@gmail.com>
2017-05-03 20:01:48 +0200

    Update plots

--- scripts/ts-sample-decomposition.R
+++ scripts/ts-sample-decomposition.R
@@ -1,7 +1,7 @@
 # Draw plot of sample data and do time series decomposition
 
-source("~/r/fiit-bp/cuper-bp/src/00-read-data.R")
-preparedData <- data.prepare(pathToFile = "~/r/fiit-bp/data/cleaned/94_UPLNE_CONVERTED.csv", measurementsPerDay = 96,
+source("src/00-read-data.R")
+preparedData <- data.prepare(pathToFile = "data/94_UPLNE_CONVERTED.csv", measurementsPerDay = 96,
                              trainingSetRange = c("2013-09-01", "2013-09-30"), testingSetRange = c("2013-07-09", "2013-07-11"))
 
 ts <- as.data.frame(stl(ts(preparedData$trainingData$value, frequency = 96), t.window = 7, s.window = "periodic", robust = TRUE)$time.series)
